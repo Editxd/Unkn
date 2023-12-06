9f60ab7ceda377c43603087003e60a20c7c3e743
Editxd <152770568+Editxd@users.noreply.github.com>
2023-12-06 20:40:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5977243533:AAH0Fbk_s09xcGi7O_ACJO1dVKrwTbYFWlk")
     API_ID = int(os.environ["API_ID", 23442389]
     API_HASH = os.environ["API_HASH", "70490ec8a810932cb5cb7f9d6a839ee0"]
-    AUTH_USERS = "5448647404 """
+    AUTH_USERS = "5489477491 """
 
